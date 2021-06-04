7cd2c8a4b8fe85e186f30b605674d758037416ba
Mariana Mohr <marianamohr@gmail.com>
2021-06-04 14:21:21 -0300

    criado Readme.md

---
+++ lista_frases/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div className="form">
+                Form
+            </div>
+        );
+    }
+}
+
+export default Form;
---
+++ lista_frases/src/components/Table.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Table extends React.Component {
+    render() {
+        return (
+            <div className="table">
+                Table
+            </div>
+        );
+    }
+}
+
+export default Table;
